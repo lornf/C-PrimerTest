dcc51de84695dc304a2767af37a82c3c5a8a15b1
lornf <lornf0459@163.com>
2019-02-11 13:09:25 +0800

    添加项目文件。

---
+++ C++PrimerTest/C++PrimerTest.cpp
@@ -0,0 +1,20 @@
+﻿// C++PrimerTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	int i = 50;
+
+	while (i <= 100)
+	{
+		sum += i++;
+	}
+
+	cout << sum << endl;
+}
+
